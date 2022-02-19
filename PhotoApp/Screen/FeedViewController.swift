4cc8162ef33dff00b436eee3326dab27fbb23586
Eser kucuker <eserkucuker@gmail.com>
2022-02-19 14:45:40 +0300

    Add getData func

--- PhotoApp/Screen/FeedViewController.swift
+++ PhotoApp/Screen/FeedViewController.swift
@@ -6,31 +6,59 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmail = [String]()
+    var command = [String]()
+    var images = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        getData()
     }
     
-
-
-
+    
+    func getData(){
+        let firestoreDatabase = Firestore.firestore()
+        firestoreDatabase.collection("Post").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else{
+                if ( snapshot?.isEmpty != true && snapshot != nil ){
+                    for document in snapshot!.documents {
+                        let documentId = document.documentID
+                        if let imageUrl =  document.get("imageUrl") as? String{
+                            self.images.append(imageUrl)
+                        }
+                        if let command = document.get("command") as? String {
+                            self.command.append(command)
+                        }
+                        if let userMail = document.get("email") as? String {
+                            self.userEmail.append(userMail)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
 }
 extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
-  
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return userEmail.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedTableViewCell
-        cell.emailTextLabel.text = "eser"
-        cell.commandTextLabel.text = "Cok Güzel Oldu"
+        cell.emailTextLabel.text = userEmail[indexPath.row]
+        cell.commandTextLabel.text = command[indexPath.row]
         return cell
     }
     
