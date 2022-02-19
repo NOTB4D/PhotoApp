3d33dd8898a91cacf020d26420b0383cc05cc01d
Eser kucuker <eserkucuker@gmail.com>
2022-02-19 14:15:00 +0300

    Add CustomFeedCell

--- PhotoApp/Screen/FeedViewController.swift
+++ PhotoApp/Screen/FeedViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,4 +20,18 @@ class FeedViewController: UIViewController {
 
 
 
+}
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedTableViewCell
+        cell.emailTextLabel.text = "eser"
+        cell.commandTextLabel.text = "Cok Güzel Oldu"
+        return cell
+    }
+    
 }
